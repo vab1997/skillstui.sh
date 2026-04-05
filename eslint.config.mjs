eeb476a144b0a51466fec4ef41a654dcb3137851
Victor Bejas <victorbejas97@gmail.com>
2026-04-05 19:01:24 -0300

    feat: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,14 @@
 import eslintPluginAstro from 'eslint-plugin-astro'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default [
   ...eslintPluginAstro.configs.recommended,
+  {
+    plugins: { 'unused-imports': unusedImports },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
   {
     ignores: [
       'dist/',
